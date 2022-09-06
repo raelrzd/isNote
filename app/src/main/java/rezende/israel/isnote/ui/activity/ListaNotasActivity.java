489980b8d8cbe8bd4f3e0d3117b5441641b9eeb6
israel <israelrezendeluiz2903@gmail.com>
2022-09-06 12:06:59 -0300

    Criado novo projeto e incluido layouts iniciais da lista e itens. First commit.

---
+++ app/src/main/java/rezende/israel/isnote/ui/activity/ListaNotasActivity.java
@@ -0,0 +1,16 @@
+package rezende.israel.isnote.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import rezende.israel.isnote.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
