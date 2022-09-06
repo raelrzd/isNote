78c3d7d798c1d6182f16608274b76dcde810decd
israel <israelrezendeluiz2903@gmail.com>
2022-09-06 15:30:35 -0300

    Realizado o Bind inicial de dados na lista de Notas, e feito tambem teste de performance.

--- app/src/main/java/rezende/israel/isnote/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/rezende/israel/isnote/ui/activity/ListaNotasActivity.java
@@ -1,10 +1,18 @@
 package rezende.israel.isnote.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import rezende.israel.isnote.R;
+import rezende.israel.isnote.dao.NotaDAO;
+import rezende.israel.isnote.model.Nota;
+import rezende.israel.isnote.ui.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -12,5 +20,14 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        ListView listaNotas = findViewById(R.id.listview);
+        NotaDAO dao = new NotaDAO();
+        for (int i = 1; i < 10000; i++){
+            dao.insere(new Nota("Titulo "+ i, "Descrição "+i));
+        }
+        List<Nota> todasNotas = dao.todos();
+        listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
+
     }
 }
